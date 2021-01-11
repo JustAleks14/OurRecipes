61060a2b9e99654c2eb74d1765482850be6045cf
JustAleks14 <aleks6_6@abv.bg>
2021-01-11 13:00:39 +0200

    Add Pages

---
+++ Web/OurRecipes.Web/Controllers/AboutUsController.cs
@@ -0,0 +1,13 @@
+﻿namespace OurRecipes.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using OurRecipes.Web.ViewModels;
+
+    public class AboutUsController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/OurRecipes.Web/Controllers/RecipeInfoController.cs
@@ -0,0 +1,13 @@
+﻿namespace OurRecipes.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using OurRecipes.Web.ViewModels;
+
+    public class RecipeInfoController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
