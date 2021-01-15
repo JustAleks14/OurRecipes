cbf7aced5322ded362bc0ce180d0a19ae388aa67
JustAleks14 <aleks6_6@abv.bg>
2021-01-15 22:57:43 +0200

    Fix css/style.css

---
+++ Web/OurRecipes.Web/Controllers/RecipesController.cs
@@ -0,0 +1,26 @@
+﻿namespace OurRecipes.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using OurRecipes.Data.Common.Repositories;
+    using OurRecipes.Data.Models;
+    using OurRecipes.Web.ViewModels;
+
+    public class RecipesController : BaseController
+    {
+        private readonly IDeletableEntityRepository<Recipe> recipesRepository;
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult AllRecipes()
+        {
+            return this.View();
+        }
+    }
+}
