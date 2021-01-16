7920a3eae0466c1689951c8b412816d5cec9768c
JustAleks14 <aleks6_6@abv.bg>
2021-01-16 10:49:03 +0200

    Add RecipesService

---
+++ Services/OurRecipes.Services.Data/RecipesService.cs
@@ -0,0 +1,36 @@
+﻿namespace OurRecipes.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using OurRecipes.Data.Common.Repositories;
+    using OurRecipes.Data.Models;
+    using OurRecipes.Services.Mapping;
+
+    public class RecipesService : IRecipesService
+    {
+        private readonly IDeletableEntityRepository<Recipe> recipesRepository;
+
+        public RecipesService(IDeletableEntityRepository<Recipe> recipesRepository)
+        {
+            this.recipesRepository = recipesRepository;
+        }
+
+        public IEnumerable<T> GetAll<T>(int page, int itemsPerPage = 12)
+        {
+            var recipes = this.recipesRepository.AllAsNoTracking()
+                .OrderByDescending(x => x.Id)
+                .Skip((page - 1) * itemsPerPage).Take(itemsPerPage)
+                .To<T>()
+                .ToList();
+            return recipes;
+        }
+
+        public int GetCount()
+        {
+            return this.recipesRepository.All().Count();
+        }
+    }
+}
--- Web/OurRecipes.Web/Controllers/RecipesController.cs
+++ Web/OurRecipes.Web/Controllers/RecipesController.cs
@@ -1,10 +1,19 @@
 ﻿namespace OurRecipes.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using OurRecipes.Services.Data;
     using OurRecipes.Web.ViewModels;
+    using OurRecipes.Web.ViewModels.Recipes;
 
     public class RecipesController : BaseController
     {
+        private readonly IRecipesService recipesService;
+
+        public RecipesController(IRecipesService recipesService)
+        {
+            this.recipesService = recipesService;
+        }
+
         public IActionResult Index()
         {
             return this.View();
@@ -13,6 +22,24 @@
         public IActionResult CreateRecipe()
         {
             return this.View();
+        }
+
+        public IActionResult AllRecipes(int id = 1)
+        {
+            if (id <= 0)
+            {
+                return this.NotFound();
+            }
+
+            const int ItemsPerPage = 12;
+            var viewModel = new RecipesListViewModel
+            {
+                ItemsPerPage = ItemsPerPage,
+                PageNumber = id,
+                RecipesCount = this.recipesService.GetCount(),
+                Recipes = this.recipesService.GetAll<AllRecipesViewModel>(id, ItemsPerPage),
+            };
+            return this.View(viewModel);
         }
     }
 }
