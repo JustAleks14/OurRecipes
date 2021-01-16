c70e5650ab2bca93ae4b2ab84988c78424621398
Hristo <andreev_h@abv.bg>
2021-01-16 15:35:46 +0200

    Working recipe info page

---
+++ Web/OurRecipes.Web.ViewModels/Recipes/RecipeInfoViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace OurRecipes.Web.ViewModels.Recipes
+{
+    using System;
+    using System.Collections.Generic;
+
+    using OurRecipes.Data.Models;
+    using OurRecipes.Data.Models.Enums;
+
+    public class RecipeInfoViewModel
+    {
+        public RecipeInfoViewModel()
+        {
+            this.Steps = new List<Step>();
+        }
+
+        public string Image { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int Time { get; set; }
+
+        public int MealCount { get; set; }
+
+        public DifficultyLevel DifficultyLevel { get; set; }
+
+        public ICollection<Step> Steps { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
